4c26f7ae036c75d9f5b3258ec475e964ea6743a0
Albert Arvidsson <albert.arvidsson@gmail.com>
2016-10-25 19:33:52 +0200

    Add basementPosition test for Day1

--- day1.js
+++ day1.js
@@ -2,21 +2,38 @@ function whatFloor () {
 }
 
 describe('Day1', () => {
-  const cases = {
-    '(())': 0,
-    '(((': 3,
-    '(()(()(': 3,
-    '))(((((': 3,
-    '())': -1,
-    '))(': -1,
-    ')))': -3,
-    ')())())': -3,
-  }
-  Object.keys(cases).map(input => {
-    const expected = cases[input]
-    describe(`given ${input}`, () => {
-      it(`returns ${expected}`, () => {
-        expect(whatFloor(input)).to.equal(expected)
+  describe('whatFloor', () => {
+    const floorCases = {
+      '(())': 0,
+      '(((': 3,
+      '(()(()(': 3,
+      '))(((((': 3,
+      '())': -1,
+      '))(': -1,
+      ')))': -3,
+      ')())())': -3,
+    }
+    Object.keys(floorCases).map(input => {
+      const expected = floorCases[input]
+      describe(`given ${input}`, () => {
+        it(`returns ${expected}`, () => {
+          expect(whatFloor(input)).to.equal(expected)
+        })
+      })
+    })
+  })
+
+  describe('basementPosition', () => {
+    const basementCases = {
+      ')': 1,
+      '()())': 5,
+    }
+    Object.keys(basementCases).forEach(input => {
+      const expected = basementCases[input]
+      describe(`given ${input}`, () => {
+        it(`returns ${expected}`, () => {
+          expect(basementPosition(input)).to.equal(expected)
+        })
       })
     })
   })
